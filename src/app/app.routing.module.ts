de4c88c0a6b86e8f19bc7f38e34613a054d08d4c
MiguelIbanezG <miguel.ibanez.gon@gmail.com>
2024-08-19 14:34:36 +0200

    aver

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: false }),
+  imports: [RouterModule.forRoot(routes, { useHash: true }),
             ModalModule.forRoot()],
   exports: [RouterModule]
 })
